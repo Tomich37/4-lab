199480f91d5545c04bdcf9c96a2ecd80a64d8110
Максим <tomich7372@gmail.com>
2018-02-05 18:39:55 +0700

    Forth redactor

--- EmployeeApp/EmployeeApp/Employee.cs
+++ EmployeeApp/EmployeeApp/Employee.cs
@@ -12,6 +12,13 @@ namespace EmployeeApp
         private string empName;
         private int empID;
         private float currPay;
+        private int empAge;
+
+        public int Age
+        {
+            get { return empAge;  }
+            set { empAge = value; }
+        }
 
         // Метод доступа (get)
         public string GetName()
@@ -34,13 +41,15 @@ namespace EmployeeApp
             }
         }
 
-
         //Конструкторы
         public Employee() {}
-        public Employee (string name, int id, float pay )
+        public Employee(string name, int id, float pay)
+            :this(name, 0, id, pay) { }
+        public Employee (string name, int age, int id, float pay )
         {
             empName = name;
             empID = id;
+            empAge = age;
             currPay = pay;
         }
 
@@ -55,6 +64,7 @@ namespace EmployeeApp
             Console.WriteLine("Name: {0}", empName);
             Console.WriteLine("ID: {0}", empID);
             Console.WriteLine("Pay: {0}", currPay);
+            Console.WriteLine("Age: {0}", empAge);
         }
     }
 }
