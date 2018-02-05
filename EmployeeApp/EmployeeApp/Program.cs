6509ed98dc5f1332cfe66d1cfb5189346dede3ab
Максим <tomich7372@gmail.com>
2018-02-05 18:20:31 +0700

    Third redactor

--- EmployeeApp/EmployeeApp/Program.cs
+++ EmployeeApp/EmployeeApp/Program.cs
@@ -10,6 +10,15 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Marvin", 456, 30000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            // Использовать методы get/set для взаимедействия с именем объекта
+            emp.SetName("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+            Console.ReadLine();
         }
     }
 }
